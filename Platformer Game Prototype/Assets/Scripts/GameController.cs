938aa158ba3e23a9651987c4e84139d333efb11c
MatheusCDias <matheuscdias2014@gmail.com>
2024-11-28 17:28:21 -0300

    fix(Player UI): Changes in interface type

--- Platformer Game Prototype/Assets/Scripts/GameController.cs
+++ Platformer Game Prototype/Assets/Scripts/GameController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
@@ -6,93 +6,96 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance;
 
-    [SerializeField, Range(0, 10)]
-    private int lifes;
-    [SerializeField, Range(0, 5)]
-    private int health;
-    [SerializeField]
-    private int coins;
-    [SerializeField]
-    private List<Sprite> fonts = new List<Sprite>();
+    [Header("Player Data")]
+    [SerializeField, Range(0, 10)] private int lifes;
+    [SerializeField, Range(0, 5)] private int health;
+    [SerializeField, Range(0, 99)] private int coins;
+    [SerializeField, Range(0, 99)] private int orbs;
 
-    [SerializeField]
-    private GameObject menu;
-    [SerializeField]
-    private GameObject uIPlayerData;
-    private bool menuCondition = false;
+    [Header("UI GameObjects")]
+    [SerializeField] private GameObject menu;
 
-    [SerializeField]
-    private Image currentHealth;
+    [Header("UI Data")]
+    [SerializeField] private Image[] currentLife = new Image[2];
+    [SerializeField] private Image currentHealth;
+    [SerializeField] private Image[] currentCoins = new Image[2];
+    [SerializeField] private Image[] currentOrbs = new Image[2];
 
-    public int Lifes
-    {
-        get { return lifes; }
-        set { lifes = value; }
-    }
+    [Header("Fonts")]
+    [SerializeField] private List<Sprite> numberFonts = new List<Sprite>();
 
-    public int Coins
-    {
-        get { return coins; }
-        set { coins = value; }
-    }
+    private Image menuFillImage; // Cache for Menu Image
 
     private void Awake()
     {
         instance = this;
-        menu.SetActive(menuCondition);
+        menuFillImage = menu.GetComponentInChildren<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        MenuMode();
-        healthUIController();
+        HandleMenuInput();
+        UpdateUI();
     }
 
-    public void healthUIController()
+    public void UpdateUI()
     {
-        switch (health)
+        // Update Life
+        if (lifes >= 0 && lifes <= 10)
         {
-            case 0:
-                currentHealth.sprite = fonts[0];
-                menu.GetComponentInChildren<Image>().fillAmount = 0;
-                break;
-            case 1:
-                currentHealth.sprite = fonts[1];
-                menu.GetComponentInChildren<Image>().fillAmount = .2f;
-                break;
-            case 2:
-                currentHealth.sprite = fonts[2];
-                menu.GetComponentInChildren<Image>().fillAmount = .4f;
-                break;
-            case 3:
-                currentHealth.sprite = fonts[3];
-                menu.GetComponentInChildren<Image>().fillAmount = .6f;
-                break;
-            case 4:
-                currentHealth.sprite = fonts[4];
-                menu.GetComponentInChildren<Image>().fillAmount = .8f;
-                break;
-            case 5:
-                currentHealth.sprite = fonts[5];
-                menu.GetComponentInChildren<Image>().fillAmount = 1;
-                break;
+            UpdateDigitUI(currentLife, lifes);
+        }
+        else
+        {
+            Debug.LogWarning("Life value is out of bounds!");
+        }
+
+        // Update Health
+        if (health >= 0 && health <= 5)
+        {
+            currentHealth.sprite = numberFonts[health];
+            menuFillImage.fillAmount = health / 5f; // Calculates the value directly as a ratio
+        }
+        else
+        {
+            Debug.LogWarning("Health value is out of bounds!");
+        }
+
+        // Update Coins
+        UpdateDigitUI(currentCoins, coins);
+
+        // Update Orbs
+        UpdateDigitUI(currentOrbs, orbs);
+    }
+
+    private void UpdateDigitUI(Image[] digitImages, int value)
+    {
+        if (digitImages.Length >= 2)
+        {
+            int tens = value / 10;
+            int units = value % 10;
+
+            digitImages[0].sprite = numberFonts[tens];
+            digitImages[1].sprite = numberFonts[units];
+        }
+        else
+        {
+            Debug.LogWarning("Digit image array is incomplete!");
         }
     }
 
     public void GetCoin()
     {
-        coins++;
+        coins = Mathf.Clamp(coins + 1, 0, 99); // Limit max value to 99
+        UpdateUI();
     }
 
-    public void MenuMode()
+    public void HandleMenuInput()
     {
-        // Open Menu
+        // Toggle menu when pressing "Fire3" button
         if (Input.GetButtonDown("Fire3"))
         {
-            menu.SetActive(!menuCondition);
-            uIPlayerData.SetActive(menuCondition);
-            menuCondition = !menuCondition;
+
         }
     }
 }
