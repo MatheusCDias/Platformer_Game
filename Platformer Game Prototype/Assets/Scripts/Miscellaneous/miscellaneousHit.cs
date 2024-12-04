f5cf224f1eef1c6f9e78a65970be196b92d74eb3
MatheusCDias <matheuscdias2014@gmail.com>
2024-12-03 22:03:41 -0300

    feat(loot box): loot box opening and instantiate coins

--- Platformer Game Prototype/Assets/Scripts/Miscellaneous/miscellaneousHit.cs
+++ Platformer Game Prototype/Assets/Scripts/Miscellaneous/miscellaneousHit.cs
@@ -40,22 +40,39 @@ public class miscellaneousHit : MonoBehaviour
             case MiscellaneousType.Vase:
                 HandleVase();
                 break;
+            case MiscellaneousType.LootBox:
+                HandleLootBox();
+                break;
         }
     }
 
     private void HandleVase()
     {
+        CoinPhysics();
+    }
+
+    public void HandleLootBox()
+    {
+        for (int i = 0; i < 3;  i++)
+        {
+            CoinPhysics();
+        }
+    }
+
+    private void CoinPhysics()
+    {
         GameObject coin = Instantiate(Resources.Load<GameObject>("Coin"), transform.position, Quaternion.identity);
         Rigidbody2D coinRb = coin.GetComponent<Rigidbody2D>();
         if (coinRb != null)
         {
-            coinRb.AddForce(new Vector2(Random.Range(-1, 1), 5), ForceMode2D.Impulse);
+            coinRb.AddForce(new Vector2(Random.Range(-1f, 1f), 5), ForceMode2D.Impulse);
         }
     }
 
     public enum MiscellaneousType
     {
         Vase,
-        Lever
+        Lever,
+        LootBox
     }
 }
